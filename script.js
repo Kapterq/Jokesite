9087e56acc6db5e800d5460e3c0bf54a92770613
Kapterq <everikoluckij@gmail.com>
2025-04-18 19:30:21 +0300

    add loader

--- script.js
+++ script.js
@@ -40,3 +40,11 @@ function generateJoke() {
     jokeText.value = selectedJoke;
   }, 500);
 }
+
+
+ window.addEventListener("load", function() {
+    let preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    preloader.style.transition = "opacity 1s ease";
+    setTimeout(() => preloader.style.display = "none", 1000);
+  });
